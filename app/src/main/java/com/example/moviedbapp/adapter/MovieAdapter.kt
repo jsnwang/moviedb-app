261916b8795da612ed956064e49d044f3a9e430f
jsnwang <jwang11211@gmail.com>
2022-02-04 15:40:15 -0500

    small edits

--- app/src/main/java/com/example/moviedbapp/adapter/MovieAdapter.kt
+++ app/src/main/java/com/example/moviedbapp/adapter/MovieAdapter.kt
@@ -2,6 +2,7 @@ package com.example.moviedbapp.adapter
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.net.toUri
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
@@ -24,15 +25,16 @@ class MovieAdapter (private val movieSearch : List<Search>) : RecyclerView.Adapt
         = movieSearch.size
 
 
-
-
     //viewholder for recyclerview adapter
    class MovieViewHolder (private val binding: MovieItemBinding)
        : RecyclerView.ViewHolder(binding.root) {
         //func that runs for each item in the recyclerview
            fun bindMovie (movie: Search) = with(binding) {
 
-                Glide.with(binding.root).load(movie.poster).override(200,300).into(ivPoster)
+                Glide.with(binding.root) //load in images using Glide
+                    .load(movie.poster)
+                    .override(200,300)
+                    .into(ivPoster)
                 tvTitle.text = movie.title //sets the text of each movie to the movie title
                 tvYear.text = movie.year //sets the year of each list item
 
