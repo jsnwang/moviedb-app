536d29564eec670c994a8728c136402a7bddae12
jsnwang <jwang11211@gmail.com>
2022-02-04 14:58:11 -0500

    created adapter viewholder

--- app/src/main/java/com/example/moviedbapp/model/adapter/MovieAdapter.kt
+++ app/src/main/java/com/example/moviedbapp/model/adapter/MovieAdapter.kt
@@ -1,6 +1,30 @@
 package com.example.moviedbapp.model.adapter
 
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.moviedbapp.databinding.FragmentHomeBinding
+import com.example.moviedbapp.databinding.MovieItemBinding
 import com.example.moviedbapp.model.resource.Search
 
 class MovieAdapter (private val movieSearch : List<Search>) {
+
+
+
+    //viewholder for recyclerview adapter
+   class MovieViewHolder (private val binding: MovieItemBinding)
+       : RecyclerView.ViewHolder(binding.root) {
+        //func that runs for each item in the recyclerview
+           fun bindMovie (movie: Search) {
+
+           }
+
+       companion object{
+           fun newInstance(parent: ViewGroup) = MovieItemBinding.inflate(
+               LayoutInflater.from(parent.context), parent, false)
+               .let { MovieViewHolder(it) }
+
+       }
+       }
+
 }
